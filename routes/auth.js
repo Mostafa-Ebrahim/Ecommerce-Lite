81ac1ef90a79926a2c8786e936c32afeeef22c64
Mostafa Ebrahim <mostafa-ebrahim@outlook.com>
2024-04-20 09:05:01 +0200

    Add user validation for signup and login

--- routes/auth.js
+++ routes/auth.js
@@ -1,17 +1,17 @@
-const express = require('express');
-
-const authController = require('../controllers/auth');
+const express = require("express");
+const authController = require("../controllers/auth");
+const validator = require("../validators/user.validator");
 
 const router = express.Router();
 
-router.get('/login', authController.getLogin);
+router.get("/login", authController.getLogin);
 
-router.get('/signup', authController.getSignup);
+router.get("/signup", authController.getSignup);
 
-router.post('/login', authController.login);
+router.post("/login", validator.login, authController.login);
 
-router.post('/signup', authController.signup);
+router.post("/signup", validator.signup, authController.signup);
 
-router.get('/logout', authController.logout);
+router.get("/logout", authController.logout);
 
 module.exports = router;
